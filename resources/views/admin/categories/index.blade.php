d494f68af8690042249c714d8d2ff149ccd07b82
Andrew Kharitonov <askildar@yandex.ru>
2019-02-03 00:24:03 +0500

    some changes

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -1,4 +1,15 @@
 @extends('admin.layouts.app_admin')
 @section ('content')
-
+<div class="container">
+    @component('admin.components.breadcrumb')
+        @slot('title')Список категорий @endslot
+        @slot('parent')Главная @endslot
+        @slot('active')Категории @endslot
+    @endcomponent
+</div>
+<hr>
+    <a href="{{route(admin.category.create)}}" class="btn btn-primary pull-right">
+        <i class="fa fa-plus-square-0"></i>Создать категорию
+    </a>
+</
 @endsection
